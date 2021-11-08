fba79d3c8b9ab48b29278433b81d5f1b6bf81c7f
Humberto Barrera <humbertobarcam@gmail.com>
2021-11-07 19:22:29 -0700

    Conexiones

---
+++ Controladores/ConexionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SistemaConsultorioMedico.Controladores
+{
+    class ConexionController
+    {
+
+        public static SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection("SERVER=LAPTOP-JQ6S98VF\\SQLEXPRESS; DATABASE=CONSULTORIO; Integrated Security=true");
+            cn.Open();
+            return cn;
+        }
+
+        public static SqlConnection Desconectar()
+        {
+            SqlConnection cn = new SqlConnection("SERVER=LAPTOP-JQ6S98VF\\SQLEXPRESS; DATABASE=CONSULTORIO; Integrated Security=true");
+            cn.Close();
+            return cn;
+        }
+
+    }
+}
