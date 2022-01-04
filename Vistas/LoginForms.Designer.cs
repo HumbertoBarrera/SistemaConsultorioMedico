8e84ff5f66469b32151e03954d4b5cc4149a72c4
Humberto Barrera <humbertobarcam@gmail.com>
2022-01-04 00:29:26 -0700

    Login de secretaria activo

--- Vistas/LoginForms.Designer.cs
+++ Vistas/LoginForms.Designer.cs
@@ -39,10 +39,8 @@ namespace SistemaConsultorioMedico
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
             this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -52,21 +50,22 @@ namespace SistemaConsultorioMedico
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(180)))));
-            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(250, 291);
+            this.panel1.Size = new System.Drawing.Size(333, 358);
             this.panel1.TabIndex = 0;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(12, 39);
+            this.pictureBox3.Location = new System.Drawing.Point(16, 48);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(229, 205);
+            this.pictureBox3.Size = new System.Drawing.Size(305, 252);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 9;
             this.pictureBox3.TabStop = false;
@@ -77,12 +76,12 @@ namespace SistemaConsultorioMedico
             this.usuariotxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.usuariotxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.usuariotxt.ForeColor = System.Drawing.Color.DimGray;
-            this.usuariotxt.Location = new System.Drawing.Point(319, 68);
+            this.usuariotxt.Location = new System.Drawing.Point(425, 84);
+            this.usuariotxt.Margin = new System.Windows.Forms.Padding(4);
             this.usuariotxt.Name = "usuariotxt";
-            this.usuariotxt.Size = new System.Drawing.Size(383, 17);
+            this.usuariotxt.Size = new System.Drawing.Size(511, 22);
             this.usuariotxt.TabIndex = 1;
             this.usuariotxt.Text = "USUARIO";
-            this.usuariotxt.TextChanged += new System.EventHandler(this.usuariotxt_TextChanged);
             this.usuariotxt.Enter += new System.EventHandler(this.usuariotxt_Enter);
             this.usuariotxt.Leave += new System.EventHandler(this.usuariotxt_Leave);
             // 
@@ -92,9 +91,10 @@ namespace SistemaConsultorioMedico
             this.contratxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.contratxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.contratxt.ForeColor = System.Drawing.Color.DimGray;
-            this.contratxt.Location = new System.Drawing.Point(319, 127);
+            this.contratxt.Location = new System.Drawing.Point(425, 156);
+            this.contratxt.Margin = new System.Windows.Forms.Padding(4);
             this.contratxt.Name = "contratxt";
-            this.contratxt.Size = new System.Drawing.Size(383, 17);
+            this.contratxt.Size = new System.Drawing.Size(511, 22);
             this.contratxt.TabIndex = 2;
             this.contratxt.Text = "CONTRASEÑA";
             this.contratxt.Enter += new System.EventHandler(this.contratxt_Enter);
@@ -105,9 +105,10 @@ namespace SistemaConsultorioMedico
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DimGray;
-            this.label1.Location = new System.Drawing.Point(463, 0);
+            this.label1.Location = new System.Drawing.Point(617, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 33);
+            this.label1.Size = new System.Drawing.Size(126, 40);
             this.label1.TabIndex = 3;
             this.label1.Text = "LOGIN";
             // 
@@ -119,9 +120,10 @@ namespace SistemaConsultorioMedico
             this.loginbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(118)))), ((int)(((byte)(126)))));
             this.loginbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loginbtn.ForeColor = System.Drawing.Color.LightGray;
-            this.loginbtn.Location = new System.Drawing.Point(317, 188);
+            this.loginbtn.Location = new System.Drawing.Point(423, 231);
+            this.loginbtn.Margin = new System.Windows.Forms.Padding(4);
             this.loginbtn.Name = "loginbtn";
-            this.loginbtn.Size = new System.Drawing.Size(408, 40);
+            this.loginbtn.Size = new System.Drawing.Size(544, 49);
             this.loginbtn.TabIndex = 4;
             this.loginbtn.Text = "ACCEDER";
             this.loginbtn.UseVisualStyleBackColor = false;
@@ -135,7 +137,7 @@ namespace SistemaConsultorioMedico
             this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
             this.lineShape2,
             this.lineShape1});
-            this.shapeContainer1.Size = new System.Drawing.Size(764, 291);
+            this.shapeContainer1.Size = new System.Drawing.Size(1019, 358);
             this.shapeContainer1.TabIndex = 5;
             this.shapeContainer1.TabStop = false;
             // 
@@ -159,29 +161,14 @@ namespace SistemaConsultorioMedico
             this.lineShape1.Y1 = 93;
             this.lineShape1.Y2 = 93;
             // 
-            // linkLabel1
-            // 
-            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(180)))));
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.LinkColor = System.Drawing.Color.DimGray;
-            this.linkLabel1.Location = new System.Drawing.Point(445, 244);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(170, 16);
-            this.linkLabel1.TabIndex = 6;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Tag = "0";
-            this.linkLabel1.Text = "¿Ha olvidado su contraseña?";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            this.linkLabel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.linkLabel1_MouseClick);
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(744, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(992, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(20, 22);
+            this.pictureBox1.Size = new System.Drawing.Size(27, 27);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -191,39 +178,23 @@ namespace SistemaConsultorioMedico
             // 
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(713, 0);
+            this.pictureBox2.Location = new System.Drawing.Point(951, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(20, 22);
+            this.pictureBox2.Size = new System.Drawing.Size(27, 27);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 8;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Purple;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(159)))), ((int)(((byte)(127)))));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(118)))), ((int)(((byte)(126)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.LightGray;
-            this.button1.Location = new System.Drawing.Point(56, 260);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 28);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "SECRE";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(764, 291);
+            this.ClientSize = new System.Drawing.Size(1019, 358);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.loginbtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.contratxt);
@@ -231,11 +202,11 @@ namespace SistemaConsultorioMedico
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.shapeContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Login";
             this.Opacity = 0.9D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Login_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Login_MouseDown);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -257,10 +228,8 @@ namespace SistemaConsultorioMedico
         private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
         private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
         private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
-        private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Button button1;
     }
 }
 
