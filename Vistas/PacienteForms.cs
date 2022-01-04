a0b74afc77b4dfbf4e2b0460f6ea4e218f4564f9
Humberto Barrera <humbertobarcam@gmail.com>
2022-01-04 12:07:23 -0700

    Correcciones

--- Vistas/PacienteForms.cs
+++ Vistas/PacienteForms.cs
@@ -209,6 +209,14 @@ namespace SistemaConsultorioMedico
             {
                 guardarPac_Btn.IdleFillColor = Color.Gray;
                 guardarPac_Btn.Enabled = false;
+                if (validarCampos())
+                {
+                    ModifPacienteBtn.Enabled = true;
+                }
+                else
+                {
+                    ModifPacienteBtn.Enabled = false;
+                }
             }
         }
 
@@ -281,7 +289,7 @@ namespace SistemaConsultorioMedico
             else
             {
                 ModifPacienteBtn.IdleFillColor = Color.Gray;
-                ModifPacienteBtn.Padding = new Padding(20, 10, 0, 0);
+                ModifPacienteBtn.Padding = new Padding(30, 10, 0, 0);
             }
         }
 
